958aa6e0c03cf4cd2632399630bcfc6eef2a275d
literat <tomas@litera.me>
2023-01-19 09:37:54 +0100

    Fix: Remove Twig comments from parsed attributes/props (refs #DS-537)

--- tests/Compiler/ComponentTagCompilerTest.php
+++ tests/Compiler/ComponentTagCompilerTest.php
@@ -22,20 +22,6 @@ class ComponentTagCompilerTest extends TestCase
         $this->assertSame('{% embed "@alias/alert.twig" with { props: {\'variable\': value,\'variableWithoutQuotes\': value} } %}{% endembed %}', $compiler->compile());
     }
 
-    public function testShouldCompileTwigVariablesWithTwigCommentSelfClosingTag(): void
-    {
-        $compiler = new ComponentTagCompiler('<Alert variable="{{value}}" {# This is comment #} variableWithoutQuotes={{value}} />', 'alias');
-
-        $this->assertSame('{% embed "@alias/alert.twig" with { props: {\'variable\': value,\'variableWithoutQuotes\': value} } %}{% endembed %}', $compiler->compile());
-    }
-
-    public function testShouldCompileTwigVariablesWithTwigComment(): void
-    {
-        $compiler = new ComponentTagCompiler('<Alert variable="{{value}}" {# This is comment #} variableWithoutQuotes={{value}}>Test</Alert>', 'alias');
-
-        $this->assertSame('{% embed "@alias/alert.twig" with { props: {\'variable\': value,\'variableWithoutQuotes\': value} } %}{% block content %}Test{% endblock %}{% endembed %}', $compiler->compile());
-    }
-
     public function testShouldCompileTwigAttributeJSXSyntax(): void
     {
         $compiler = new ComponentTagCompiler('<Alert number={12} value={ aaa } isBool={false} />', 'alias');
@@ -48,5 +34,57 @@ class ComponentTagCompilerTest extends TestCase
         $compiler = new ComponentTagCompiler('<Alert number={ true ? 12 : 10 } />', 'alias');
 
         $this->assertSame('{% embed "@alias/alert.twig" with { props: {\'number\': true ? 12 : 10} } %}{% endembed %}', $compiler->compile());
+    }
+
+    /**
+     * @dataProvider twigCommentsDataProvider
+     */
+    public function testShouldCompileTwigVariablesWithTwigComment(string $component, string $expected): void
+    {
+        $compiler = new ComponentTagCompiler($component, 'alias');
+
+        $this->assertSame($expected, $compiler->compile());
+    }
+
+    /**
+     * @return array<string, string[]>
+     */
+    public function twigCommentsDataProvider(): array
+    {
+        return [
+            // component, expected
+            'comment in self-closing component' => [
+                '<Alert variable="{{value}}" {# This is comment #} variableWithoutQuotes={{value}} />',
+                '{% embed "@alias/alert.twig" with { props: {\'variable\': value,\'variableWithoutQuotes\': value} } %}{% endembed %}',
+            ],
+            'multiple comments in self-closing component' => [
+                '<Alert {# This is comment #} variable="{{value}}" {# This is second comment #} variableWithoutQuotes={{value}} />',
+                '{% embed "@alias/alert.twig" with { props: {\'variable\': value,\'variableWithoutQuotes\': value} } %}{% endembed %}',
+            ],
+            'comment in pair component' => [
+                '<Alert variable="{{value}}" {# This is comment #} variableWithoutQuotes={{value}}>Test</Alert>',
+                '{% embed "@alias/alert.twig" with { props: {\'variable\': value,\'variableWithoutQuotes\': value} } %}{% block content %}Test{% endblock %}{% endembed %}',
+            ],
+            'multiple comments in pair component' => [
+                '<Alert {# This is comment #} variable="{{value}}" {# This is second comment #} variableWithoutQuotes={{value}}>Test</Alert>',
+                '{% embed "@alias/alert.twig" with { props: {\'variable\': value,\'variableWithoutQuotes\': value} } %}{% block content %}Test{% endblock %}{% endembed %}',
+            ],
+            'comment out prop in self-closing component' => [
+                '<Alert {# variable="{{value}}" #} variableWithoutQuotes={{value}} />',
+                '{% embed "@alias/alert.twig" with { props: {\'variableWithoutQuotes\': value} } %}{% endembed %}',
+            ],
+            'comment out prop in pair component' => [
+                '<Alert {# variable="{{value}}" #} variableWithoutQuotes={{value}}>Test</Alert>',
+                '{% embed "@alias/alert.twig" with { props: {\'variableWithoutQuotes\': value} } %}{% block content %}Test{% endblock %}{% endembed %}',
+            ],
+            'empty comment in self-closing component' => [
+                '<Alert variable="{{value}}" {# #} variableWithoutQuotes={{value}} />',
+                '{% embed "@alias/alert.twig" with { props: {\'variable\': value,\'variableWithoutQuotes\': value} } %}{% endembed %}',
+            ],
+            'empty comment in pair component' => [
+                '<Alert variable="{{value}}" {#  #} variableWithoutQuotes={{value}}>Test</Alert>',
+                '{% embed "@alias/alert.twig" with { props: {\'variable\': value,\'variableWithoutQuotes\': value} } %}{% block content %}Test{% endblock %}{% endembed %}',
+            ],
+        ];
     }
 }
